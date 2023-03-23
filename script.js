33cd8deb91f1364989414f978c232b468f20c71c
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-03-23 11:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+	var i= 0;
+	var j = str.length-1;
+
+	while(i<j)
+		{
+			if(str.charAt(i)!=str.charAt(j))
+			{
+				return false
+			}
+			i++;
+			j--;
+		}
+	return true;
 
 }
 module.exports = palindrome
